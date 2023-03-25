fa41ed28edb9e277ae019c7bc961862a73231bab
Kong-E <kng001016@dgu.ac.kr>
2023-03-25 18:54:54 +0900

    push role and content 수정

--- backend/index.js
+++ backend/index.js
@@ -25,44 +25,51 @@ app.post("/fortuneTell", async function (req, res) {
   let { userMessages, assistantMessages } = req.body;
   console.log(userMessages);
   console.log(assistantMessages);
-  const completion = await openai.createChatCompletion({
-    model: "gpt-3.5-turbo",
-    messages: [
-      {
-        role: "system",
-        content:
-          "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다.  당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
-      },
-      {
-        role: "user",
-        content:
-          "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다.  당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
-      },
-      {
-        role: "assistant",
-        content:
-          "안녕하세요, 저는 챗도지입니다. 무엇을 도와드릴까요? 운세, 삶의 방향성, 사랑, 직업, 건강 등 여러 가지 주제에서 질문 을 받고 있습니다. 질문해 주시면 최대한 자세하고 명확하게 답변해 드리도록 노력하겠습니다.",
-      },
-      { role: "user", content: "오늘의 운세가 뭐야?" },
-    ],
-  });
 
-  while (userMessages.length != 0 && assistantMessages.length != 0) {
+  let messages = [
+    {
+      role: "system",
+      content:
+        "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다.  당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
+    },
+    {
+      role: "user",
+      content:
+        "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다.  당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다.",
+    },
+    {
+      role: "assistant",
+      content:
+        "안녕하세요, 저는 챗도지입니다. 무엇을 도와드릴까요? 운세, 삶의 방향성, 사랑, 직업, 건강 등 여러 가지 주제에서 질문 을 받고 있습니다. 질문해 주시면 최대한 자세하고 명확하게 답변해 드리도록 노력하겠습니다.",
+    },
+  ];
+
+  while (userMessages.length != 0 || assistantMessages.length != 0) {
     if (userMessages.length != 0) {
       messages.push(
-        '{role: "user", content: "' +
-          String(userMessages.shift()).replace(/\n/g, "") +
-          '"}'
+        JSON.parse(
+          '{"role": "user", "content": "' +
+            String(userMessages.shift()).replace(/\n/g, "") +
+            '"}'
+        )
       );
     }
     if (assistantMessages.length != 0) {
       messages.push(
-        '{role: "assistant", content: "' +
-          String(assistantMessages.shift()).replace(/\n/g, "") +
-          '"}'
+        JSON.parse(
+          '{"role": "assistant", "content": "' +
+            String(assistantMessages.shift()).replace(/\n/g, "") +
+            '"}'
+        )
       );
     }
   }
+
+  const completion = await openai.createChatCompletion({
+    model: "gpt-3.5-turbo",
+    messages: messages,
+  });
+
   let fortune = completion.data.choices[0].message["content"];
   console.log(fortune);
   res.json({ assistant: fortune }); //json 응답으로 받기
